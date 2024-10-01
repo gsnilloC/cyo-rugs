53772d73625e3701b4917b94b78848f8577d9748
Collins Gichohi <cgichohi2018@gmail.com>
2024-10-01 14:28:21 -0700

    added password page to cover page

--- backend/server.js
+++ backend/server.js
@@ -6,22 +6,17 @@ const path = require("path");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
 connectDB();
 
-// Serve static files from the React app
 app.use(express.static(path.join(__dirname, "../build")));
 
-// Define a route for the root URL
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
-// Handle any other routes by sending the index.html file
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
