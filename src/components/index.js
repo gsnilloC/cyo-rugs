f322cf1692f86a3d71eeaa6f35445cf630f35d4b
Collins Gichohi <cgichohi2018@gmail.com>
2024-10-03 14:22:41 -0700

    still changing file names

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { default as Home } from "./Home";
+export { default as Home } from "./home";
 export { default as Shop } from "./shop";
 export { default as Cart } from "./cart";
 export { default as Request } from "./request";
