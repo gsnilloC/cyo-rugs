a774e35ac00eab1a03ca5763791121f24b49f1f6
Collins Gichohi <cgichohi2018@gmail.com>
2024-11-27 12:21:10 -0800

    Fix missing module and specify Node.js version

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,5 @@ export { default as Shop } from "./shop";
 export { default as Cart } from "./cart";
 export { default as Request } from "./request";
 export { default as About } from "./about";
-export { default as PasswordPage } from "./passwordPage";
 export { default as Product } from "./product";
 export { default as RequestList } from "./requestList";
